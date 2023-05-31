a09ce460846e9ee7fd550353f82571878a25a09b
alexspandro <129217632+alexspandro@users.noreply.github.com>
2023-05-31 15:14:56 -0400

    Update culminating.py

--- culminating.py
+++ culminating.py
@@ -1,3 +1,4 @@
 print("Congratulations, you met your goal of", WeightGoal , "lbs!"
 print("Keep up the good work, you are ", CurrentWeight - WeightGoal , "lbs away from your goal!"
-
+plt.axhline(y=0.5, color='r', linestyle='-')
+plt.show()
