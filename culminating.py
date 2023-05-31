1db86a5ac93da2a73afbac013ed281e986b036d1
alexspandro <129217632+alexspandro@users.noreply.github.com>
2023-05-31 15:34:28 -0400

    Update culminating.py

--- culminating.py
+++ culminating.py
@@ -1,3 +1,15 @@
+import matplotlib as plt
+
+
+x_axis = ['value_1', 'value_2', 'value_3', ...]
+y_axis = ['value_1', 'value_2', 'value_3', ...]
+
+plt.plot(x_axis, y_axis)
+plt.title('title name')
+plt.xlabel('x_axis name')
+plt.ylabel('y_axis name')
+plt.show()
+
 print("Congratulations, you met your goal of", WeightGoal , "lbs!"
 print("Keep up the good work, you are ", CurrentWeight - WeightGoal , "lbs away from your goal!"
 plt.axhline(y=0.5, color='r', linestyle='-')
