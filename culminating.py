d2f9c0df8a7f6edf681feb9bffcbff18649ae107
alexspandro <129217632+alexspandro@users.noreply.github.com>
2023-06-02 15:23:52 -0400

    Update culminating.py

--- culminating.py
+++ culminating.py
@@ -1,14 +1,19 @@
 import matplotlib as plt
+from datetime import datetime, timedelta
+CurrentWeight = int(input("What is your current weight?"))
+WeightGoal = int(input("What is the weight goal you are trying to achieve?"))
+TrackDuration = int(input("How many days would you like to track your weight for?"))
 
-// CurrentWeight = int input("What is your current weight?")
-// WeightGoal = int input("What is the weight goal you are trying to achieve?")
-x_axis = ['value_1', 'value_2', 'value_3', ...]
-y_axis = ['value_1', 'value_2', 'value_3', ...]
+dates = [datetime.now() + timedelta(days=i) for i in range(TrackDuration)] #keeps count of how many days will be tracked
 
-plt.plot(x_axis, y_axis)
+weights = [CurrentWeight] * TrackDuration
+
+plt.plot(dates, weights)
 plt.title('Tracker Graph')
 plt.xlabel('Date')
 plt.ylabel('Weight')
+plt.legend(['Weight', 'WeightGoal'])
+plt.grid(True)
 plt.axhline(y=WeightGoal , color='r', linestyle='-')
 plt.show()
 
