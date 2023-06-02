5960db7edce2d9106ba531eed256eac42f9e53fe
alexspandro <129217632+alexspandro@users.noreply.github.com>
2023-06-01 23:24:53 -0400

    Update culminating.py

--- culminating.py
+++ culminating.py
@@ -1,16 +1,17 @@
 import matplotlib as plt
 
-
+// CurrentWeight = int input("What is your current weight?")
+// WeightGoal = int input("What is the weight goal you are trying to achieve?")
 x_axis = ['value_1', 'value_2', 'value_3', ...]
 y_axis = ['value_1', 'value_2', 'value_3', ...]
 
 plt.plot(x_axis, y_axis)
-plt.title('title name')
-plt.xlabel('x_axis name')
-plt.ylabel('y_axis name')
+plt.title('Tracker Graph')
+plt.xlabel('Date')
+plt.ylabel('Weight')
+plt.axhline(y=WeightGoal , color='r', linestyle='-')
 plt.show()
 
 print("Congratulations, you met your goal of", WeightGoal , "lbs!"
 print("Keep up the good work, you are ", CurrentWeight - WeightGoal , "lbs away from your goal!"
-plt.axhline(y=0.5, color='r', linestyle='-')
-plt.show()
+
