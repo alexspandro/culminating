92fb81b528b73190ab3adbf45200285cc4c6be76
alexspandro <129217632+alexspandro@users.noreply.github.com>
2023-06-05 19:35:16 -0400

    Update culminating.py

--- culminating.py
+++ culminating.py
@@ -1,12 +1,19 @@
 import matplotlib as plt
-
+folder = os.getcwd()
 WeightGoal = int(input("What is the weight goal you are trying to achieve?"))
+print("The tracking timeframe is set to 7 days.")
 
-TrackDuration = int(input("How many days would you like to track your weight for?"))
+oneW = int("What is your weight on the 1st day?"))
+twoW = int("What is your weight on the 2nd day?"))
+threeW = int("What is your weight on the 3rd day?"))
+fourW = int("What is your weight on the 4th day?"))
+fiveW = int("What is your weight on the 5th day?"))
+sixW = int("What is your weight on the 6th day?"))
+sevenW = int("What is your weight on the 7th day?"))
 
 dates = [day 1, day 2, day 3, day 4, day 5, day 6, day 7]
+weights = [40, 45, 70, 20, 60, 65, 40]
 
-weights = [, , , , , , ]
 plt.scatter(dates, weights)
 plt.title('Tracker Graph')
 plt.xlabel('Day')
@@ -16,7 +23,22 @@ plt.grid(True)
 plt.axhline(y=WeightGoal , color='r', linestyle='-')
 plt.show()
 
-print("Congratulations, you met your goal of", WeightGoal , "lbs!"
-print("Keep up the good work, you are ", CurrentWeight - WeightGoal , "lbs away from your goal!"
+print("Your information is currently being saved to a file named 'userWeightInfo.csv'.")
+folderName = input("Which existing folder would you like to assign the file to? ")
+fileCreateNew = folder + folderName + "\\userWeightInfo.csv"
 
-with open 
+#file is opened, written on, and closed
+with open ("userWeightinfo.csv", "w") as file:
+  file.writelines("NEW WEIGHT PROFILE (LBS)\n-------------")
+  file.writelines("Day 1: " + oneW + "\n")
+  file.writelines("Day 2: " + twoW + "\n")
+  file.writelines("Day 3: " + threeW + "\n")
+  file.writelines("Day 4: " + fourW + "\n")
+  file.writelines("Day 5: " + fiveW + "\n")
+  file.writelines("Day 6: " + sixW + "\n")
+  file.writelines("Day 7: " + sevenW + "\n")
+  
+print("FILE SAVED IN FOLDER:", folderName)
+
+print("Congratulations, you met your goal of", WeightGoal , "lbs!")
+print("Keep up the good work, you are ", sevenW - WeightGoal , "lbs away from your goal!")
