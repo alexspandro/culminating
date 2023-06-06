0f451fc236050ce8b380ab83af01c6de044e86cb
alexspandro <129217632+alexspandro@users.noreply.github.com>
2023-06-06 17:35:58 -0400

    Update culminating.py

--- culminating.py
+++ culminating.py
@@ -1,43 +1,33 @@
 import os
 import matplotlib.pyplot as plt
-
 folder = os.getcwd()
-WeightGoal = int(input("What is the weight goal you are trying to achieve?"))
+
+weightGoal = int(input("What is the weight goal, in pounds(lbs), are you trying to achieve?"))
 print("The tracking timeframe is set to 7 days.")
-
-dates = ['day 1', 'day 2', 'day 3', 'day 4', 'day 5', 'day 6', 'day 7']
-weights = [40, 45, 70, 20, 60, 65, 40]
-
-plt.scatter(dates, weights)
-plt.title('Tracker Graph')
-plt.xlabel('Day')
-plt.ylabel('Weight')
-plt.legend(['Weight', 'WeightGoal (red)'])
-plt.grid(True)
-plt.axhline(y=WeightGoal, color='r', linestyle='-')
-plt.show()
 
 weightDay = []
 for i in range(1, 8):
-weight = int(input("how much did you weight on day {i} ?")
-#uses an appended list to ask user's weight for 7 days
+    weight = int(input(f"How much did you weigh on day {i}? "))
+    weightDay.append(weight)
 
-print("Your information is currently being saved to a file named 'userWeightInfo.csv'.")
+print("Your information is currently being saved to a file named 'userWeightInfo.png'.")
 folderName = input("Which existing folder would you like to assign the file to? ")
-fileCreateNew = folder + folderName + "\\userWeightInfo.csv"
+fileCreateNew = folder + folderName + "\\userWeightInfo.png"
+    
+dates = ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7']
+plt.scatter(dates, weightDay)
+plt.title('Tracker Graph')
+plt.xlabel('Day')
+plt.ylabel('Weight (lbs)')
+plt.legend(['Weight', 'weightGoal'])
+plt.grid(True)
+plt.axhline(y=weightGoal, color='r', linestyle='-')
+plt.show()
+plt.savefig('userWeightInfo.png')
 
-#file is opened, written on, and closed
-with open ("userWeightinfo.csv", "w") as file:
-  file.writelines("NEW WEIGHT PROFILE (LBS)\n-------------")
-  file.writelines("Day 1: " + oneW + "lbs\n")
-  file.writelines("Day 2: " + twoW + "lbs\n")
-  file.writelines("Day 3: " + threeW + "lbs\n")
-  file.writelines("Day 4: " + fourW + "lbs\n")
-  file.writelines("Day 5: " + fiveW + "lbs\n")
-  file.writelines("Day 6: " + sixW + "lbs\n")
-  file.writelines("Day 7: " + sevenW + "lbs\n")
-  
-print("FILE SAVED IN FOLDER:", folderName)
-
-print("Congratulations, you met your goal of", WeightGoal , "lbs!")
-print("Keep up the good work, you are ", sevenW - WeightGoal , "lbs away from your goal!")
+# the part that needs to be fixed
+daySeven = weight[6]  # Access the 7th element (index 6) of the list
+if daySeven == weightGoal or daySeven < weightGoal:
+  print("Congratulations, you met your goal of", weightGoal , "lbs!")
+else:
+  print("Keep up the good work, you are ", daySeven - weightGoal , "lbs away from your goal!")
