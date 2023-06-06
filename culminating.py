38f52781de35221aec8a3159efe17b194f7de3db
alexspandro <129217632+alexspandro@users.noreply.github.com>
2023-06-06 14:46:49 -0400

    Update culminating.py

--- culminating.py
+++ culminating.py
@@ -3,13 +3,13 @@ folder = os.getcwd()
 WeightGoal = int(input("What is the weight goal you are trying to achieve?"))
 print("The tracking timeframe is set to 7 days.")
 
-oneW = int("What is your weight on the 1st day?"))
-twoW = int("What is your weight on the 2nd day?"))
-threeW = int("What is your weight on the 3rd day?"))
-fourW = int("What is your weight on the 4th day?"))
-fiveW = int("What is your weight on the 5th day?"))
-sixW = int("What is your weight on the 6th day?"))
-sevenW = int("What is your weight on the 7th day?"))
+oneW = int(input("What is your weight on the 1st day?"))
+twoW = int(input("What is your weight on the 2nd day?"))
+threeW = int(input("What is your weight on the 3rd day?"))
+fourW = int(input("What is your weight on the 4th day?"))
+fiveW = int(input("What is your weight on the 5th day?"))
+sixW = int(input("What is your weight on the 6th day?"))
+sevenW = int(input("What is your weight on the 7th day?"))
 
 dates = [day 1, day 2, day 3, day 4, day 5, day 6, day 7]
 weights = [40, 45, 70, 20, 60, 65, 40]
@@ -30,13 +30,13 @@ fileCreateNew = folder + folderName + "\\userWeightInfo.csv"
 #file is opened, written on, and closed
 with open ("userWeightinfo.csv", "w") as file:
   file.writelines("NEW WEIGHT PROFILE (LBS)\n-------------")
-  file.writelines("Day 1: " + oneW + "\n")
-  file.writelines("Day 2: " + twoW + "\n")
-  file.writelines("Day 3: " + threeW + "\n")
-  file.writelines("Day 4: " + fourW + "\n")
-  file.writelines("Day 5: " + fiveW + "\n")
-  file.writelines("Day 6: " + sixW + "\n")
-  file.writelines("Day 7: " + sevenW + "\n")
+  file.writelines("Day 1: " + oneW + "lbs\n")
+  file.writelines("Day 2: " + twoW + "lbs\n")
+  file.writelines("Day 3: " + threeW + "lbs\n")
+  file.writelines("Day 4: " + fourW + "lbs\n")
+  file.writelines("Day 5: " + fiveW + "lbs\n")
+  file.writelines("Day 6: " + sixW + "lbs\n")
+  file.writelines("Day 7: " + sevenW + "lbs\n")
   
 print("FILE SAVED IN FOLDER:", folderName)
 
