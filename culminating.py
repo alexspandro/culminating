c390a0d129ab6fdbc681ca24286ef07eeed02c61
alexspandro <129217632+alexspandro@users.noreply.github.com>
2023-06-07 15:08:00 -0400

    Update culminating.py

--- culminating.py
+++ culminating.py
@@ -1,6 +1,6 @@
 import os
 import matplotlib.pyplot as plt
-folder = os.getcwd()
+# folder = os.getcwd()
 
 weightGoal = int(input("What is the weight goal, in pounds(lbs), are you trying to achieve?"))
 print("The tracking timeframe is set to 7 days.")
@@ -9,11 +9,11 @@ weightDay = []
 for i in range(1, 8):
     weight = int(input(f"How much did you weigh on day {i}? "))
     weightDay.append(weight)
-
+'''
 print("Your information is currently being saved to a file named 'userWeightInfo.png'.")
 folderName = input("Which existing folder would you like to assign the file to? ")
 fileCreateNew = folder + folderName + "\\userWeightInfo.png"
-    
+'''    
 dates = ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7']
 plt.scatter(dates, weightDay)
 plt.title('Tracker Graph')
@@ -23,7 +23,7 @@ plt.legend(['Weight', 'weightGoal'])
 plt.grid(True)
 plt.axhline(y=weightGoal, color='r', linestyle='-')
 plt.show()
-plt.savefig('userWeightInfo.png')
+# plt.savefig('userWeightInfo.png')
 
 # the part that needs to be fixed
 daySeven = weight[6]  # Access the 7th element (index 6) of the list
